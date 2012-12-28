9fdbde96e467379b0690479a8b56b447b4fb3a1f
Eoin Campbell <github@eoincampbell.net>
2012-12-28 13:57:18 +0000

    Information
    Fixed Asssembly Info... Added placeholders for Readme & Licence.

--- Parachute/Properties/AssemblyInfo.cs
+++ Parachute/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Parachute")]
-[assembly: AssemblyDescription("A Sql Server Schema Deployment Tool")]
+[assembly: AssemblyDescription("A Database Schema & Script Deployment Tool")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
@@ -26,17 +26,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("448032ff-c950-4712-9a90-6855ffbf9a10")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("Parachute v1.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("Parachute v0.1 Beta")]
 
